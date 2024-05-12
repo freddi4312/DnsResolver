555fe84456a809fcf4a2b9ed9dc5f5c9186d0d29
freddi4312 <freddi.pervyshev@gmail.com>
2024-05-12 05:49:11 +0300

    Add script for getting required libraries

---
+++ get_libraries.bat
@@ -0,0 +1,5 @@
+mkdir lib
+cd lib
+git clone https://github.com/mfontanini/libtins.git
+git clone https://github.com/Neargye/magic_enum.git
+git clone --branch v0.8.x https://github.com/fpagliughi/sockpp.git
